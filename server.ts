7c9a52c0866f8a44be3b831199937aec220c4d5c
Davi <davi.campos689@gmail.com>
2024-02-16 14:32:54 -0300

    feat: add prisma connection to database

--- server.ts
+++ server.ts
@@ -2,11 +2,15 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import app from "./src/application/app";
+import prisma from "./src/infrastructure/db/prisma";
 
 const PORT = Number(process.env.PORT) ?? 3000;
 
 async function main() {
   try {
+
+    prisma.$connect()
+
     await app.listen({
       port: PORT,
     });
